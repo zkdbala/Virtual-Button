ca292161cf9a869ff90e25d69af61c7658b145d2
Anshita Bala <anshita.bala2020@vitstudent.ac.in>
2021-11-18 03:54:28 +0530

    Added Project

---
+++ Virtual Caed/Assets/VirtualCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class VirtualCard : MonoBehaviour,IVirtualButtonEventHandler
+{
+    public GameObject spherego, cubego;
+    VirtualButtonBehaviour vrb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+        cubego.SetActive(true);
+        spherego.SetActive(false);
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+   
+public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubego.SetActive(false);
+        spherego.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubego.SetActive(true);
+        spherego.SetActive(false);
+
+    }
+}
